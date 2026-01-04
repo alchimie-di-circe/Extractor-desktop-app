fd44df9dc7ca876684a2d0defb45023e0e770a1f
Alex Tombollillo <bookslookfor@gmail.com>
2026-01-04 21:38:45 +0000

    chore: Remove unused MakerZIP import and add arch=all flag to make script

--- forge.config.ts
+++ forge.config.ts
@@ -1,6 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDMG } from "@electron-forge/maker-dmg";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
